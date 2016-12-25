47fe437058229607e252e1e5194ca9678d1fdb07
imedia-development <imedia.inf@gmail.com>
2016-12-25 19:03:30 +0700

    update depedencies

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+
+
 var Config = {
   // Contents of this file will be send to the client
   "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
 
-  "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
+  "serverip":   process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  "serverport": process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || '8080',
   
   "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
   "protocol":   'ws://',
